75421e9b05e3b6171a0b7fe647b4afc724cab131
haytastan <45230816+haytastan@users.noreply.github.com>
2019-09-13 11:54:27 +0300

    test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -15,8 +15,10 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  //directConnect: true,
+  directConnect: false,
+  //baseUrl: 'http://localhost:4200/',
+  seleniumAddress: 'http://localhost:4444/wd/hub/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
